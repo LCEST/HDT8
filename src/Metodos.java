e2b02d5cbf434bd91692a2df26fff37d69ab5c68
Gustavo de Leon <gdeleontobias@gmail.com>
2018-04-05 20:56:18 -0600

    Implementacion de metodos, PriorityQueque y VectorHeap

---
+++ src/Metodos.java
@@ -0,0 +1,48 @@
+
+import java.io.IOException;
+import java.util.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Metodos  {
+    
+    public void agregar(VectorHeap<Paciente> heap) throws IOException{
+       
+        Lector lector = new Lector();
+        String[] listPac = lector.getText();
+        for (int i = 0; i < listPac.length; i++){
+            String[] datPac = listPac[i].split(", ");
+            Paciente new_pac = new Paciente(datPac[0],datPac[1],datPac[2]);
+            heap.add(new_pac);
+        }
+        
+    }
+   public String print(VectorHeap<Paciente> heap){ 
+       int size = heap.size();
+       int contador =1;
+       String lista ="";         
+       while(contador <= size) {
+            String msjTemp=heap.getFirst().toString();
+            System.out.println(heap.getFirst().toString());
+            lista=lista+msjTemp;
+            heap.remove();
+            contador++;
+       }
+       return lista;
+   }
+   public void quitar(VectorHeap<Paciente> heap){
+       
+   }
+}
+
+    
+    
+
